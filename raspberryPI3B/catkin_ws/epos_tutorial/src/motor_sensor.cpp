4358c57f117c2e5686f90e62f3ca488cca47f975
Jeong Hye-mi <hyami222@gmail.com>
2019-06-12 22:55:24 +0900

    Add files via upload

---
+++ raspberryPI3B/catkin_ws/epos_tutorial/src/motor_sensor.cpp
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+#include "epos_tutorial/realVel.h"
+
+void commandCallback(const epos_tutorial::realVel::ConstPtr& msg)
+{
+    ROS_INFO("Motor Velocity (rpm): %d, %d, %d, %d", msg->realVel[0], msg->realVel[1], msg->realVel[2], msg->realVel[3]);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "motor_sensor");
+
+    ros::NodeHandle n;
+
+    ros::Subscriber sub = n.subscribe("measure", 1000, commandCallback);
+
+    ros::spin();
+
+    return 0;
+}
